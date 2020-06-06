7f114f3c29f55bc932d119505b0f8afc78f6b431
zeynep öztürkmen <zynpoztrkmn@hotmail.com>
2020-06-06 21:46:18 +0300

    Add project files.

---
+++ WindowsServiceRabbitMQ/Consumer.cs
@@ -0,0 +1,59 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WindowsServiceRabbitMQ
+{
+    public class Consumer
+    {
+        private readonly RabbitMQService _rabbitMQService;
+
+        public Consumer(string queueName)
+        {
+            _rabbitMQService = new RabbitMQService();
+
+            using (var connection = _rabbitMQService.GetRabbitMQConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    var consumer = new EventingBasicConsumer(channel);
+                    // Received event'i sürekli listen modunda olacaktır.
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body.ToArray());
+
+                        string path = AppDomain.CurrentDomain.BaseDirectory + "\\News";
+                        if (!Directory.Exists(path))
+                        {
+                            Directory.CreateDirectory(path);
+                        }
+                        string filepath = AppDomain.CurrentDomain.BaseDirectory + "\\News\\ServiceLog_" + DateTime.Now.Date.ToShortDateString().Replace('/', '_') + ".txt";
+                        if (!File.Exists(filepath))
+                        {
+                            using (StreamWriter sw = File.CreateText(filepath))
+                            {
+                                sw.WriteLine(message);
+                            }
+                        }
+                        else
+                        {
+                            using (StreamWriter sw = File.AppendText(filepath))
+                            {
+                                sw.WriteLine(message);
+                            }
+                        }
+                    };
+                    //queue: Hangi Queue’nun mesajları alınacak ise.
+                    //noAck: True olarak set edildiği taktirde, consumer mesajı aldığı zaman otomatik olarak mesaj Queue’dan silinecektir. Eğer Queue üzerinden silinmesini istemiyor iseniz, False olarak set etmeniz gerekmektedir.
+                    channel.BasicConsume(queueName, true, consumer);
+                }
+            }
+        }
+    }
+}
---
+++ WindowsServiceRabbitMQ/Service1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WindowsServiceRabbitMQ
+{
+    public partial class Service1 : ServiceBase
+    {
+        public Service1()
+        {
+            InitializeComponent();
+        }
+
+        Thread trd;
+        private static readonly string _queueName = "ZEYNEPOZTURKMEN";
+        private static Consumer _consumer;
+
+        protected override void OnStart(string[] args)
+        {
+            trd = new Thread(new ThreadStart(WriteToFile));
+            trd.IsBackground = false;
+            trd.Start();
+        }
+        public void WriteToFile()
+        {
+            while (true)
+            {
+                _consumer = new Consumer(_queueName);
+
+                Thread.Sleep(2000);
+            }
+        }
+        protected override void OnStop()
+        {
+        }
+    }
+}
